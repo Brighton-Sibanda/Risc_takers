e585e2a4274b2a4e3cf51ee61b3842b4b520152c
Angela Davis <angeladavis@Angelas-MacBook-Pro.local>
2024-01-10 12:41:33 -0600

    prime number test bench

---
+++ testbenches/prime_numbers.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    num = int(sys.argv[1])
+    if num > 1:
+        for i in range(2, int(num/2)+1):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
+
+if __name__ == "__main__":
+    main()
