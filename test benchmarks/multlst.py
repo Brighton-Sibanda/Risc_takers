d3b7450316c36d0fc6ff6508983d33456e9a6a2f
heesungasong <102690584+heesungasong@users.noreply.github.com>
2024-01-12 15:48:19 -0600

    Create multlst.py

---
+++ test benchmarks/multlst.py
@@ -0,0 +1,7 @@
+"""
+Parameters:
+- lst (list): The input list.
+- multiplier (int or float): The number to multiply each element by.
+"""
+def multiply_list_elements(lst, multiplier):
+    return [element * multiplier for element in lst]
