07fce02af01e6b01599926b92ecd546a50c27c79
Angela Davis <angeladavis@dhcp-10-105-59-70.wireless.northwestern.private>
2024-01-29 18:42:30 -0600

    continuous + structural booth

--- risc_takers.v
+++ risc_takers.v
@@ -374,9 +374,8 @@ module PipelinedCPU(halt, clk, rst);
         signed_tempReg = {{20{fetch_reg_ex[31]}}, fetch_reg_ex[31:20]};
         temp_addrReg = signed_tempReg + Rdata1_fin;
         loadHalt = 1;
-        next_stages[0] = 0;
-        next_stages[1] = 0;
-        next_stages[2] = 0; // new addition
+        stages[0] = 0;
+        stages[1] = 0;
         PCReg = PCRegEx + 4;
         if (fetch_reg_ex[14:12] == 3'b000)
         begin // LB
@@ -507,11 +506,9 @@ module PipelinedCPU(halt, clk, rst);
       else if (fetch_reg_ex[6:0] == `OPCODE_JALR)
       begin
         signed_tempReg = Rdata1_fin;
-        temp_addrReg   = 32'hfffffffe & (signed_tempReg + {{20{fetch_reg_ex[31]}}, fetch_reg_ex[31:20]});
+        temp_addrReg   = signed_tempReg + {{20{fetch_reg_ex[31]}}, fetch_reg_ex[31:20]};
         $display("halt6");
         haltFlagReg   = temp_addrReg[0] | temp_addrReg[1];
-        $display("stored address is %8x, PC is %8x", signed_tempReg, PCReg);
-        $display("address to be jumped to is %8x", temp_addrReg);
 
         // additional code:
         RdstRegMem_next = Rdst_reg_ex;
@@ -762,18 +759,25 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
   input [2:0] 	 func;
   input [6:0] 	 auxFunc;
 
-  // Place your code here
+  // Only supports add and subtract ,, commenting the code below, do we need it?
+  // wire [31:0] 	 addSub;
+
+  // assign addSub = (auxFunc == 7'b0100000) ? (opA - opB) : (opA + opB);
+  // assign out = (func == 3'b000) ? addSub : 32'hXXXXXXXX;
+
+  // execution code for single cycle as in lab 1.
   reg [31:0] myOutput;
   wire signed [31:0] signedopA = $signed(opA);
   wire signed [31:0] signedopB = $signed(opB);
   reg enable = 1'b0;
   wire [31:0] mul_out;
-  Booth_multiplication booth1(signedopA, signedopB, mul_out, enable);
 
-  always @(*)
+  BM bm1(.signedopA(signedopA), .signedopB(signedopB), .out(mul_out), .enable(enable));
+
+  always @*
   begin
+
     //check each bit of the input to classify them
-    enable = 1'b0;
     case (func[2:0])
       //Arithmetic
       3'b000:
@@ -783,14 +787,14 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
         end
         else if (auxFunc[0] == 1'b1)
         begin
+          $display("multiplication answer is %8x", mul_out);
           enable = 1'b1;
         end
         else
         begin
           myOutput = opA + opB;
         end
-
-      //SRL and SRA
+      // SRL and SRA
       3'b101:
         if (auxFunc[5] == 1'b1)
         begin
@@ -819,52 +823,121 @@ module ExecutionUnit(out, opA, opB, func, auxFunc);
   assign out = enable ? mul_out :  myOutput ;
 endmodule // ExecutionUnit
 
+module BM(signedopA, signedopB, out, enable);
 
-module Booth_multiplication (signedopA, signedopB, out, enable);
-
-  input signed [31:0] signedopA, signedopB;
-  input enable;
-  output signed [31:0] out;
-  reg [31:0] out_result;
-  reg [64:0] accumulator = 64'b0;
-  reg [64:0] addition_val;
-  reg signed sign;
-  reg signed [31:0] cmpval;
-  integer i;
-
-  assign out = accumulator[31:0];
-  always @(*) begin
-      if (enable == 1'b1) begin
-        addition_val = {signedopA, {32'b0}};
-        for (i = 0; i < 32; i = i + 1) begin
-            sign = accumulator[63];
-            cmpval = signedopB[i-1];
-            if (i == 1'b0) begin
-              cmpval = 32'b0;
-            end
-            if (signedopB[i] == 1'b0 && cmpval == 1'b0) begin
-              accumulator = accumulator + 1'b0;
-            end
-            else if (signedopB[i] == 1'b0 && cmpval == 1'b1) begin
-              accumulator = accumulator + addition_val;
-            end
-            else if (signedopB[i] == 1'b1 && cmpval == 1'b0) begin 
-              accumulator = accumulator - addition_val;
-            end 
-            else if (signedopB[i] == 1'b1 && cmpval == 1'b1) begin 
-              accumulator = accumulator + 1'b0;
-            end 
-
-            accumulator = accumulator >> 1;
-            accumulator[63] = sign;  
-        end
-        //sign = accumulator[31];
-        // accumulator = accumulator >> 1;
-        $display("multiplication answer is %8x", accumulator);
-        //accumulator[31] = sign; 
-      end
-  end 
+  input wire signed [31:0] signedopA;
+  input wire signed [31:0] signedopB;
+  input wire enable;
+  output wire signed [31:0] out;
 
 
+  wire signed [31:0] signedopB_neg;
+  wire signed [64:0] partial_products [31:0];
+  wire signed [63:0] product_accumulator;
+  wire signed [63:0] shifted;
+
+  assign signedopB_neg = ~signedopB + 1;
+
+  assign partial_products[0]  = signedopA[0]  ? {{32{1'b0}}, signedopA} + (signedopB_neg << 32) : {{32{1'b0}}, signedopA};
+
+  assign partial_products[1]  = (partial_products[0][0] == partial_products[0][1])  ? (partial_products[0] >>> 1)  : (partial_products[0][1]) ? 
+  ((partial_products[0] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[0] >>> 1));
+
+  assign partial_products[2]  = (partial_products[1][0] == partial_products[1][1])  ? (partial_products[1] >>> 1)  : (partial_products[1][1]) ? 
+  ((partial_products[1] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[1] >>> 1));
+
+  assign partial_products[3]  = (partial_products[2][0] == partial_products[2][1])  ? (partial_products[2] >>> 1)  : (partial_products[2][1]) ? 
+  ((partial_products[2] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[2] >>> 1));
+
+  assign partial_products[4]  = (partial_products[3][0] == partial_products[3][1])  ? (partial_products[3] >>> 1)  : (partial_products[3][1]) ? 
+  ((partial_products[3] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[3] >>> 1));
+
+  assign partial_products[5]  = (partial_products[4][0] == partial_products[4][1])  ? (partial_products[4] >>> 1)  : (partial_products[4][1]) ? 
+  ((partial_products[4] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[4] >>> 1));
+
+  assign partial_products[6]  = (partial_products[5][0] == partial_products[5][1])  ? (partial_products[5] >>> 1)  : (partial_products[5][1]) ? 
+  ((partial_products[5] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[5] >>> 1));
+
+  assign partial_products[7]  = (partial_products[6][0] == partial_products[6][1])  ? (partial_products[6] >>> 1)  : (partial_products[6][1]) ? 
+  ((partial_products[6] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[6] >>> 1));
+
+  assign partial_products[8]  = (partial_products[7][0] == partial_products[7][1])  ? (partial_products[7] >>> 1)  : (partial_products[7][1]) ? 
+  ((partial_products[7] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[7] >>> 1));
+
+  assign partial_products[9]  = (partial_products[8][0] == partial_products[8][1])  ? (partial_products[8] >>> 1)  : (partial_products[8][1]) ? 
+  ((partial_products[8] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[8] >>> 1));
+
+  assign partial_products[10]  = (partial_products[9][0] == partial_products[9][1])  ? (partial_products[9] >>> 1)  : (partial_products[9][1]) ? 
+  ((partial_products[9] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[9] >>> 1));
+
+  assign partial_products[11]  = (partial_products[10][0] == partial_products[10][1])  ? (partial_products[10] >>> 1)  : (partial_products[10][1]) ? 
+  ((partial_products[10] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[10] >>> 1));
+
+  assign partial_products[12]  = (partial_products[11][0] == partial_products[11][1])  ? (partial_products[11] >>> 1)  : (partial_products[11][1]) ? 
+  ((partial_products[11] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[11] >>> 1));
+
+  assign partial_products[13]  = (partial_products[12][0] == partial_products[12][1])  ? (partial_products[12] >>> 1)  : (partial_products[12][1]) ?
+  ((partial_products[12] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[12] >>> 1));
+
+  assign partial_products[14]  = (partial_products[13][0] == partial_products[13][1])  ? (partial_products[13] >>> 1)  : (partial_products[13][1]) ? 
+  ((partial_products[13] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[13] >>> 1));
+
+  assign partial_products[15]  = (partial_products[14][0] == partial_products[14][1])  ? (partial_products[14] >>> 1)  : (partial_products[14][1]) ? 
+  ((partial_products[14] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[14] >>> 1));
+
+  assign partial_products[16]  = (partial_products[15][0] == partial_products[15][1])  ? (partial_products[15] >>> 1)  : (partial_products[15][1]) ? 
+  ((partial_products[15] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[15] >>> 1));
+
+  assign partial_products[17]  = (partial_products[16][0] == partial_products[16][1])  ? (partial_products[16] >>> 1)  : (partial_products[16][1]) ? 
+  ((partial_products[16] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[16] >>> 1));
+
+  assign partial_products[18]  = (partial_products[17][0] == partial_products[17][1])  ? (partial_products[17] >>> 1)  : (partial_products[17][1]) ? 
+  ((partial_products[17] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[17] >>> 1));
+
+  assign partial_products[19]  = (partial_products[18][0] == partial_products[18][1])  ? (partial_products[18] >>> 1)  : (partial_products[18][1]) ? 
+  ((partial_products[18] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[18] >>> 1));
+  
+   assign partial_products[20]  = (partial_products[19][0] == partial_products[19][1])  ? (partial_products[19] >>> 1)  : (partial_products[19][1]) ? 
+  ((partial_products[19] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[19] >>> 1));
+  
+   assign partial_products[21]  = (partial_products[20][0] == partial_products[20][1])  ? (partial_products[20] >>> 1)  : (partial_products[20][1]) ? 
+  ((partial_products[20] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[20] >>> 1));
+  
+   assign partial_products[22]  = (partial_products[21][0] == partial_products[21][1])  ? (partial_products[21] >>> 1)  : (partial_products[21][1]) ? 
+  ((partial_products[21] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[21] >>> 1));
+  
+   assign partial_products[23]  = (partial_products[22][0] == partial_products[22][1])  ? (partial_products[22] >>> 1)  : (partial_products[22][1]) ? 
+  ((partial_products[22] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[22] >>> 1));
+  
+   assign partial_products[24]  = (partial_products[23][0] == partial_products[23][1])  ? (partial_products[23] >>> 1)  : (partial_products[23][1]) ? 
+  ((partial_products[23] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[23] >>> 1));
+  
+   assign partial_products[25]  = (partial_products[24][0] == partial_products[24][1])  ? (partial_products[24] >>> 1)  : (partial_products[24][1]) ? 
+  ((partial_products[24] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[24] >>> 1));
+  
+   assign partial_products[26]  = (partial_products[25][0] == partial_products[25][1])  ? (partial_products[25] >>> 1)  : (partial_products[25][1]) ? 
+  ((partial_products[25] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[25] >>> 1));
+  
+   assign partial_products[27]  = (partial_products[26][0] == partial_products[26][1])  ? (partial_products[26] >>> 1)  : (partial_products[26][1]) ? 
+  ((partial_products[26] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[26] >>> 1));
+  
+   assign partial_products[28]  = (partial_products[27][0] == partial_products[27][1])  ? (partial_products[27] >>> 1)  : (partial_products[27][1]) ? 
+  ((partial_products[27] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[27] >>> 1));
+  
+   assign partial_products[29]  = (partial_products[28][0] == partial_products[28][1])  ? (partial_products[28] >>> 1)  : (partial_products[28][1]) ? 
+  ((partial_products[28] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[28] >>> 1));
+  
+   assign partial_products[30]  = (partial_products[29][0] == partial_products[29][1])  ? (partial_products[29] >>> 1)  : (partial_products[29][1]) ? 
+  ((partial_products[29] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[29] >>> 1));
+  
+   assign partial_products[31]  = (partial_products[30][0] == partial_products[30][1])  ? (partial_products[30] >>> 1)  : (partial_products[30][1]) ? 
+  ((partial_products[30] >>> 1)+(signedopB_neg << 32)) : ((signedopB << 32) + (partial_products[30] >>> 1));
+  
+
+
+  assign shifted = partial_products[31];
+
+
+  assign out = shifted >> 1;
 
 endmodule
